073344868e71574dc8a3b0b9ad563b941e1e11f6
etianm <155981372+etianm@users.noreply.github.com>
2024-01-13 00:45:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b;
+        Console.Write("Enter a: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter b: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        if (a % b != 0)
+        {
+            Console.WriteLine("Impartirea nu poate fi efectuata in mod exact.");
+        }
+        else
+        {
+            Console.WriteLine($"{a} / {b} = {a / b}");
+        }
+    }
+}
+
+
